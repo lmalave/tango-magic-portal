e375798a8c699fabd57dc7604f52a72ccaca6434
lmalave <lmalave@hugeinc.com>
2016-04-09 15:19:29 -0400

    Adding scene transitions

---
+++ Assets/DebugOutput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugOutput : MonoBehaviour {
+
+	public GameObject DebugText;
+
+	public TextMesh DebugTextMesh;
+	// Use this for initialization
+	void Start () {
+		DebugTextMesh = DebugText.GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DebugTextMesh.text = "Position: " + transform.position;
+		if (transform.position.z >= 1) {
+			Application.LoadLevel("VRScene");
+		}
+	}
+}
